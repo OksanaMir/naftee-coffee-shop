023bcd627121f1f10354ca69586ec685c202492a
Oleksandra Shapovalova <shapovalovaova@gmail.com>
2021-05-09 22:55:15 +0200

    DatoCMS connected and landing page continued

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,6 @@ export default function Home() {
                 return <ProductOverView key={product.id} data={product} />;
               })}
           </section>
-          <ProductOverView />
         </main>
       </Layout>
     </div>
