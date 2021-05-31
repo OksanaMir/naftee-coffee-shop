d8dfecd34b7ebae6f1c630c771dd0aa8581f10eb
Oleksandra Shapovalova <shapovalovaova@gmail.com>
2021-05-31 21:35:49 +0200

    selectors from datacms

--- components/product/ProductOverView.js
+++ components/product/ProductOverView.js
@@ -16,7 +16,6 @@ export function ProductOverView({ data }) {
   const [form] = Form.useForm();
   const [selectsData, setSelectsData] = useState({});
   const [weightSelect, setWeightSelect] = useState(250);
-
   const [methodSelect, setMethodSelect] = useState('espresso');
   const [quantity, setQuantity] = useState(1);
 
