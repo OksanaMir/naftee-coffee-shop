6410e4430f469ce7b940550ccf5bc764e5ce7673
Oksana Mironova <kq3hip13z@gmail.com>
2021-06-02 15:13:07 +0200

    Teaser included

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
 import { useEffect, useState } from 'react';
 import { request } from '../lib/datoCMS';
-
+import { Loader } from '../components/ loader/Loader';
+import { isMobile } from 'react-device-detect';
+import { ProductTeaser } from '../components/product/ProductTeaser';
 import { Carousel } from 'antd';
 import { LandingPageAboutUs } from '../components/landingPage/LandingPageAboutUs';
 import { Layout } from '../components/layout/Layout';
-import { Loader } from '../components/ loader/Loader';
 import { ProductOverView } from '../components/product/ProductOverView';
 import styles from '../styles/Index.module.scss';
 
@@ -35,34 +36,42 @@ export default function Index() {
       </Head>
 
       <Layout>
-        <main>
-          <div className={styles.mainPhoto}>
-            <LandingPageAboutUs />
-          </div>
-          {isLoading ? (
-            <Loader />
-          ) : (
-            <section className={styles.main}>
-              <Carousel accessibility={true} arrows={true}>
-                {data?.allProducts?.map((product) => {
-                  return (
-                    <div
-                      style={{
-                        width: '100%',
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                      }}
-                      key={product.id}
-                    >
-                      <ProductOverView key={product.id} data={product} />
-                    </div>
-                  );
-                })}
-              </Carousel>
-            </section>
-          )}
-        </main>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <main>
+            <div className={styles.mainPhoto}>
+              <LandingPageAboutUs />
+            </div>
+            {isMobile ? (
+              <section className={styles.mainMobile}>
+                <Carousel accessibility={true} arrows={true}>
+                  {data?.allProducts?.map((product) => {
+                    return (
+                      <div
+                        style={{
+                          width: '100%',
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                        }}
+                        key={product.id}
+                      >
+                        <ProductOverView key={product.id} data={product} />
+                      </div>
+                    );
+                  })}
+                </Carousel>
+              </section>
+            ) : (
+              <section className={styles.mainDesctop}>
+                {data?.allProducts?.map((product) => (
+                  <ProductTeaser key={product.id} data={product} />
+                ))}
+              </section>
+            )}
+          </main>
+        )}
       </Layout>
     </div>
   );
@@ -77,6 +86,14 @@ const PRODUCT_QUERY = `query ProductQuery($locale: SiteLocale){
     id
     quantityWeight 
     horizontalProductView {
+      alt
+      id
+      url
+      title
+      width
+      height
+    }
+    productBanner {
       alt
       id
       url
