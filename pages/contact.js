f411d580db9fda26151add84d5358d80bf7e18c8
Oleksandra Shapovalova <shapovalovaova@gmail.com>
2021-06-02 12:53:27 +0200

    DatoCMS on pages - contacts and about

--- pages/contact.js
+++ pages/contact.js
@@ -1,23 +1,35 @@
 import Head from 'next/head';
-import styles from '../styles/Contact.module.scss';
-import { ContactForm } from '../components/form/forms/ContactForm';
 import { useTranslation } from 'react-i18next';
+import { request } from '../../lib/datoCMS';
+import { useEffect, useState } from 'react';
+import { ContactForm } from '../components/form/forms/ContactForm';
+
 import { Layout } from '../components/layout/Layout';
 
+import styles from '../styles/Contact.module.scss';
+
 export default function ContactFormPage() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    request({
+      query: CONTACT_QUERY,
+      variables: { locale: i18n.language },
+    }).then((response) => {
+      setData(response);
+    });
+  }, [i18n.language]);
+
   return (
     <>
       <Head>Contact</Head>
       <Layout>
         <section className={styles.contactContainer}>
-          <h1>{t('contacts.contact')}</h1>
-          <p>{t('contacts.text1')}</p>
-          <p>{t('contacts.adress1')}</p>
-          <p>{t('contacts.adress2')}</p>
-          <p>{t('contacts.adress3')}</p>
-          <p>{t('contacts.phone')}</p>
-          <p>{t('contacts.text2')}</p>
+          <h1>{data?.contact?.headline}</h1>
+          <p>{data?.contact?.information}</p>
+          <p>{data?.contact?.callToAction}</p>
+
           <p className={styles.instruction}>{t('contacts.instruction')}</p>
 
           <ContactForm />
@@ -26,3 +38,11 @@ export default function ContactFormPage() {
     </>
   );
 }
+const CONTACT_QUERY = `query ContactQuery($locale: SiteLocale)
+{
+  contact(locale:$locale){
+    headline
+   information
+   callToAction
+  }
+ }`;
