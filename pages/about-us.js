c68afb7ce99dedacf71f610b0a113e7366c96e8d
sascheup <80477974+sascheup@users.noreply.github.com>
2021-06-02 15:32:52 +0200

    22 design main (#30)
    * main design
    
    * main design
    
    * changing
    
    * selectors from datacms
    
    * selector weight, price and quintity
    
    * DatoCMS on pages - contacts and about

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,12 +1,23 @@
 import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
-
+import { request } from '../../lib/datoCMS';
+import { useEffect, useState } from 'react';
 import { Layout } from '../components/layout/Layout';
 
 import styles from '../styles/About.module.scss';
 
 export default function AboutUs() {
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    request({
+      query: ABOUT_QUERY,
+      variables: { locale: i18n.language },
+    }).then((response) => {
+      setData(response);
+    });
+  }, [i18n.language]);
 
   return (
     <>
@@ -19,10 +30,17 @@ export default function AboutUs() {
           <h1>About us.</h1>
           <section className={styles.aboutSection}>
             <img alt="about us" src="../assets/aboutUs.jpg" />
-            <p>{t('aboutUs.paragraph')} </p>
+            <p>{data?.about?.history}</p>
           </section>
         </section>
       </Layout>
     </>
   );
 }
+
+const ABOUT_QUERY = `query AboutQuery($locale: SiteLocale){
+ 
+  about(locale: $locale) {
+    history
+  }
+}`;
