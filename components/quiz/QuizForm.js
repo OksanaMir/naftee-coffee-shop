4ca67590042d8fdd6ee3cdeb08e34bd0f942314f
Vera <46943647+VVeRRa@users.noreply.github.com>
2021-06-06 21:24:51 +0200

    31 brushing up styles (#39)
    * Global and local styles
    
    * commit after rebase
    
    Co-authored-by: Oksana Mironova <kq3hip13z@gmail.com>
    Co-authored-by: VeraKasperova <vera.kasperova@isfgroup.cz>

--- components/quiz/QuizForm.js
+++ components/quiz/QuizForm.js
@@ -1,11 +1,10 @@
-import { Button, Form, Popover, Radio } from "antd";
-import { useEffect, useRef, useState } from "react";
-import { i18n, useTranslation } from "react-i18next";
-import { QuizNavBar } from "./QuizNavBar";
-import { QuizBlockBtns } from "./QuizBlockBtns";
-import { Loader } from "../ loader/Loader";
+import {Button, Form, Popover, Radio} from "antd";
+import {useEffect, useRef, useState} from "react";
+import {useTranslation} from "react-i18next";
+import {QuizNavBar} from "./QuizNavBar";
+import {QuizBlockBtns} from "./QuizBlockBtns";
 import styles from "../../styles/QuizForm.module.scss";
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
 
 const layout = {
   labelCol: {
@@ -21,36 +20,34 @@ const tailLayout = {
     span: 16,
   },
 };
-export function QuizForm({ onFinished, answers, setAnswers, quiz }) {
+export function QuizForm({ onFinished, quiz }) {
   const { t } = useTranslation();
   const router = useRouter();
   const [form] = Form.useForm();
   const [quizItemIndex, setQuizItemIndex] = useState(0);
-  const [chosenAnswerValue, setChosenAnswerValue] = useState(
-    quiz?.[quizItemIndex]?.option[0]
-  );
+
   const [chosenAnswerIndex, setChosenAnswerIndex] = useState(0);
   const ref = useRef(null);
   const [selectedAnswers, setSelectedAnswers] = useState([]);
-  const [chosenAmount, setChosenAmount] = useState(0);
-  const [chosenSort, setChosenSort] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-  //{ choice1: sort1, choice2: sort2, choice3: sort3, choice4: sort4 };
+
+  const isLastQuizItem = quizItemIndex === quiz.length - 1;
+
+
   useEffect(() => {
-    setChosenAnswerValue(quiz[quizItemIndex]?.option[0]);
     form.setFieldsValue({ options: quiz[quizItemIndex]?.option[0] });
   }, [quizItemIndex]);
+
   useEffect(() => {
     setQuizItemIndex(0);
-    setChosenAnswerValue(quiz?.[0]?.option[0]);
     form.setFieldsValue({
       options: quiz?.[0]?.option[0],
     });
   }, [quiz]);
+
   const chooseOption = (e) => {
-    setChosenAnswerValue(e.target.value);
     setChosenAnswerIndex(e.target.index);
   };
+
   const returnCorrectAnswer = () => {
     return typeof quiz?.[quizItemIndex]?.recommendation?.[chosenAnswerIndex] ===
       "string" ||
@@ -59,22 +56,16 @@ export function QuizForm({ onFinished, answers, setAnswers, quiz }) {
       ? quiz?.[quizItemIndex]?.recommendation?.[chosenAnswerIndex]
       : quiz?.[quizItemIndex]?.recommendation?.[chosenAnswerIndex]?.id;
   };
-  const isLastQuizItem = quizItemIndex === quiz.length - 1;
+
+
   const onContinue = () => {
-    // console.log('on-continue');
-    // console.log('sort counts', chosenSort);
-    // console.log('ans-1', answers, chosenAnswerValue, quizItemIndex);
     setSelectedAnswers([...selectedAnswers, returnCorrectAnswer()]);
     if (isLastQuizItem) {
       return;
     }
-    // console.log('ans-2', answers);
-    // setChosenAnswerValue(quiz[quizItemIndex].option[0].value);
     setQuizItemIndex(quizItemIndex + 1);
   };
-  // const chooseInstruction = (e) => {
-  //   setChosenAnswerIndex(e.target.index);
-  // };
+
   const sendAnswers = () => {
     const results = [...selectedAnswers, returnCorrectAnswer()];
 
@@ -85,26 +76,19 @@ export function QuizForm({ onFinished, answers, setAnswers, quiz }) {
         ? results[3]
         : results[4];
 
-    console.log("send", results, "sort", sort);
     onFinished({
       package: results[2],
       method: results[0],
       coffeeSort: sort,
     });
   };
+
   const FormQuestion = () => {
     return <h1>{quiz?.[quizItemIndex]?.question}</h1>;
   };
-  // console.log(
-  //   quiz?.[quizItemIndex]?.instruction?.[chosenAnswerIndex],
-  //   'popover',
-  // );
-  // console.log(chosenAnswerIndex, 'answer');
+
   return (
     <>
-      {isLoading ? (
-        <Loader />
-      ) : (
         <div className={styles.form}>
           <Form form={form} {...layout}>
             <QuizNavBar
@@ -123,7 +107,8 @@ export function QuizForm({ onFinished, answers, setAnswers, quiz }) {
                     id={`popoverArea${index}`}
                   >
                     {quiz?.[quizItemIndex]?.instruction?.[chosenAnswerIndex] ? (
-                      <Popover
+                        <div className={styles.popover}>
+                        <Popover
                         placement="right"
                         title={undefined}
                         content={
@@ -146,6 +131,7 @@ export function QuizForm({ onFinished, answers, setAnswers, quiz }) {
                           {option}
                         </Radio>
                       </Popover>
+                        </div>
                     ) : (
                       <Radio key={option.id} value={option} index={index}>
                         {option}
@@ -167,7 +153,6 @@ export function QuizForm({ onFinished, answers, setAnswers, quiz }) {
             )}
           </Form>
         </div>
-      )}
     </>
   );
 }
